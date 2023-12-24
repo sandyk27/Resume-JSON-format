15c90350c79984a64e29901d77f6a6f65f39eed6
sandyK27 <sk5158839@gmail.com>
2023-12-24 18:46:31 +0530

    The two files are added sucessfully

---
+++ index.js
@@ -0,0 +1,57 @@
+let myresume = {
+    "basics":{
+        "name":"SanthoshKumar",
+        "email id":"sk5158839@gmail.com",
+        "mobile":7010835650,
+        "degree":"B.cs",
+
+        "location":{
+            "Address":"1/136,Mainraod karuvazaikarai,mayiladuthurai(dt)",
+            "pincode":"609301",
+            "city":"Mayiladuthurai",
+            "state":"TamilNadu",
+            "country":"India",
+        },
+        "profile":[
+            {
+               "website" : "https://www.linkedin.com/in/santhoshkumar98/",
+                "website" : "https://github.com/sandyk27",
+            }
+        ]
+    },
+    "work":[
+        {
+            "Company":"Hinduja Leyaland Finance LTD",
+            "Experience":"2Years",
+            "Current Role":"Collection Officer",
+            "DOJ":"21.12.2021",
+        },
+    ],
+    "Education":[
+        {
+            "Collage":"Gurunanck Collage of arts and science",
+            "Department":"Visual Communication",
+            "Study Time":"FullTime",
+            "Batch":"2016",
+            "Batch End":"2019",
+        },
+    ],
+    "Skills":[
+        {
+            "name":"HTML,Javascript,Excel",
+            "leval":"Entry Leval",
+        },
+    ],
+    "Language":[
+        {
+            "Languages":"Tamil,English",
+        },
+    ],
+    "Entertiment":[
+        {
+            "Entertiment":"Games,Movie,Travel",
+        }
+    ]
+}
+
+console.log(myresume);
